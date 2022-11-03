f9e28f12cb260ec6ca7b97f4496895817e44a483
firstlikethis <69718787+firstlikethis@users.noreply.github.com>
2022-11-04 00:58:33 +0700

    Update sql

--- controller/inven.sql
+++ controller/inven.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2022 at 06:33 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Nov 03, 2022 at 06:57 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,15 +37,35 @@ CREATE TABLE `tb_members` (
   `password` varchar(255) NOT NULL,
   `tel` varchar(10) NOT NULL,
   `login_last` datetime NOT NULL,
-  `status` enum('member','admin','','') NOT NULL DEFAULT 'member'
+  `status` enum('member','admin','','') NOT NULL DEFAULT 'member',
+  `image_user` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tb_members`
 --
 
-INSERT INTO `tb_members` (`id_member`, `firstname`, `lastname`, `ip`, `department`, `email`, `password`, `tel`, `login_last`, `status`) VALUES
-(10, 'try', 'trand', '::1', 'it', 'g@gmail.com', '4f442542000951e6acad7a3115d514f116c8c7ab0045edf96cfee39ae4f9c0c9', '0982389122', '2022-11-03 12:11:59', 'member');
+INSERT INTO `tb_members` (`id_member`, `firstname`, `lastname`, `ip`, `department`, `email`, `password`, `tel`, `login_last`, `status`, `image_user`) VALUES
+(10, 'try', 'trand', '::1', 'it', 'g@gmail.com', '4f442542000951e6acad7a3115d514f116c8c7ab0045edf96cfee39ae4f9c0c9', '0982389122', '2022-11-04 00:11:19', 'admin', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_products`
+--
+
+CREATE TABLE `tb_products` (
+  `id_products` int(11) NOT NULL,
+  `name_product` varchar(50) NOT NULL,
+  `serial_number` varchar(50) NOT NULL,
+  `ip_address` varchar(50) NOT NULL,
+  `mac_address` varchar(30) NOT NULL,
+  `boxlan` varchar(20) NOT NULL,
+  `departments` varchar(50) NOT NULL,
+  `name_users` varchar(50) NOT NULL,
+  `floors` varchar(10) NOT NULL,
+  `remark` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -58,6 +78,12 @@ ALTER TABLE `tb_members`
   ADD PRIMARY KEY (`id_member`);
 
 --
+-- Indexes for table `tb_products`
+--
+ALTER TABLE `tb_products`
+  ADD PRIMARY KEY (`id_products`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,6 +92,12 @@ ALTER TABLE `tb_members`
 --
 ALTER TABLE `tb_members`
   MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id', AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tb_products`
+--
+ALTER TABLE `tb_products`
+  MODIFY `id_products` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

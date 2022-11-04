16cc48012010a11d287fb7138ac5c2432311a2ad
Chakkri A <69718787+firstlikethis@users.noreply.github.com>
2022-11-04 16:48:26 +0700

    4-11-2022 nothing change

--- admin_dashboard/admin_includes/sidebar.php
+++ admin_dashboard/admin_includes/sidebar.php
@@ -4,8 +4,6 @@
    <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
      <div class="brand-logo">
       <a href="index.html">
-       <img src="../lib/assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
-       <h5 class="logo-text">Dashboard Admin</h5>
      </a>
    </div>
    <ul class="sidebar-menu do-nicescrol">
